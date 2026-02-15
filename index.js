6a490b887751e5eed7b275e81774f890dbaf1051
Ali İhsan Özkaya <alihsanozkaya27@gmail.com>
2026-02-15 16:19:32 +0300

    small fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.json({ status: "ok" }));
 
+startTelegramBot();
+
 const port = process.env.PORT || 3003;
 app.listen(port, '0.0.0.0', () => console.log(`Bot başlatıldı ${port}`));
-
-startTelegramBot();
