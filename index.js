2ef08cc85e2bf5d43ee7ac91b2768cb8629b3a11
Ali İhsan Özkaya <alihsanozkaya27@gmail.com>
2026-02-15 16:18:42 +0300

    bot express added

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+import express from "express";
 import "dotenv/config";
 import { startTelegramBot } from "./platforms/telegramBot.js";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.json({ status: "ok" }));
+
+const port = process.env.PORT || 3003;
+app.listen(port, '0.0.0.0', () => console.log(`Bot başlatıldı ${port}`));
 
 startTelegramBot();
